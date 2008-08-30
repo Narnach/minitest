4cdf764b0f5800e87b91ac466928382046ce8967
Wes Oldenbeuving <narnach@gmail.com>
2008-08-30 19:55:37 +0200

    Refactored specs to use a helper to set the load path

--- spec/minitest_spec.rb
+++ spec/minitest_spec.rb
@@ -1,4 +1,4 @@
-$LOAD_PATH.unshift File.expand_path(File.join(File.dirname(__FILE__),'..','lib'))
+require File.join(File.dirname(__FILE__),'spec_helper')
 require 'minitest'
 
 describe Minitest do
